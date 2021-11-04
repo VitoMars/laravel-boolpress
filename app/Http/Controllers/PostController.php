edc5a0b9175452fbbb5ef3716e5383dac165bcef
Vito <vito95v@gmail.com>
2021-11-04 18:02:42 +0100

    fix name

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view("guest.post.index", compact("posts"));
+        return view("guest.posts.index", compact("posts"));
     }
 
     /**
