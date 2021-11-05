281b726f4cc038635221613820f20b55e3f413a5
Vito <vito95v@gmail.com>
2021-11-05 13:43:24 +0100

    fix controller

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.dashboard')
 
 @section('content')
-   @dump($posts)
    <ul>
       @foreach ($posts as $post)
-          <li><a href="{{ route("admin.posts.show", $post->id) }}">{{ $post->title }}</a></li>
+          <li><a href="{{ route("admin.posts.show", $post->slug) }}">{{ $post->title }}</a></li>
       @endforeach
    </ul>
 @endsection
