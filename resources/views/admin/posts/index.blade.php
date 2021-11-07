983a71e6ce8e2c7dfa0c98aabad8c81a91d5837f
Vito <vito95v@gmail.com>
2021-11-07 19:54:51 +0100

    Aggiunto modale

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -43,15 +43,42 @@
                {{-- Modify --}}
                <td><a href="{{ route("admin.posts.edit", $post["id"]) }}" class="btn btn-warning">Modify</a></td>
                {{-- Delete --}}
-               <td>
+               <td><a class="btn btn-danger" data-toggle="modal" data-target="#deletePost{{$post->id}}" href="#deletePost">Delete</a></td>
+               {{-- <td>
                   <form action="{{ route("admin.posts.destroy", $post["id"]) }}" method="post" class="d-inline-block">
                      @csrf
                      @method("DELETE")
                      <button type="submit" onclick="window.confirmDelete()" class="btn btn-danger">Delete</button>
                   </form>
-               </td>
+               </td> --}}
             </tr>
-          @endforeach
+
+            <!-- Modal -->
+            <div class="modal fade" id="deletePost{{$post->id}}" tabindex="-1" aria-labelledby="deletePostLabel" aria-hidden="true">
+               <div class="modal-dialog">
+                  <div class="modal-content">
+                     <div class="modal-header">
+                        <h5 class="modal-title" id="deletePostLabel">Delete confirmation: Post # {{$post->id}}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                           <span aria-hidden="true">&times;</span>
+                        </button>
+                  </div>
+                  <div class="modal-body">
+                     <p>You are about to <strong>permanently</strong> delete the post from the database.</p>
+                     <p>Are you sure you want to delete it?</p>
+                     <p><strong>The action cannot be reversed</strong></p>
+                  </div>
+                  <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                     <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST">
+                         @csrf
+                         @method('DELETE')
+                         <button type="submit" class="btn btn-danger">Delete</button>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         @endforeach
       </tbody>
    </table>
 @endsection 
