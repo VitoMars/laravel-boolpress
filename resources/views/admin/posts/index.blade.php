d095be26813deb067793a0a862b3aa0ab526f787
Vito <vito95v@gmail.com>
2021-11-07 18:50:24 +0100

    fix index

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,45 +1,37 @@
 @extends('layouts.dashboard')
 
-
 @section('content')
-    <div class="container">
-      <h1>Lista dei Comics</h1>
-      {{-- @dump($comics) --}}
-      <div class="row">
-        <div class="col-12">
-          <table class="table table-striped">
-            <thead>
-              <tr class="">
-                <th scope="col">#</th>
-                <th scope="col">Title</th>
-                <th scope="col">Slug</th>
-                <th scope="col">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-
-              @foreach ($posts as $post)
-              <tr>
-                <th scope="row">{{$post["id"]}}</th>
-                <td>{{$post["title"]}}</td>
-                <td>{{$post["slug"]}}</td>
-                <td>{{$post["type"]}}</td>
-                {{-- Provare senza ID --}}
-                <td><a href="{{ route("admin.posts.show", $post["id"]) }}" class="btn btn-info">Details</a></td>
-                <td><a href="{{ route("admin.posts.edit", $post["id"]) }}" class="btn btn-warning">Modify</a></td>
-                <td>
-                  <form action="{{ route("admin.posts.destroy", $post["id"]) }}" method="post">
-                    @csrf
-                    @method("DELETE")
-                    <button type="submit" onclick="window.confirmDelete()" class="btn btn-danger" data-bs-toggle="modal">Delete</button>
+   <h1>Lista dei Posts</h1>
+   <table class="table table-striped">
+      <thead> 
+         <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Actions</th>
+         </tr>
+      </thead>
+      {{-- Body --}}
+      <tbody>
+         @foreach ($posts as $post)
+            <tr>
+               <td scope="row"> {{ $post["id"] }}</td>
+               <td>{{ $post["title"] }}</td>
+               <td>{{ $post["slug"] }}</td>
+               {{-- Details --}}
+               <td><a href="{{ route("admin.posts.show", $post["id"]) }}" class="btn btn-info">Details</a></td>
+               {{-- Modify --}}
+               <td><a href="{{ route("admin.posts.edit", $post["id"]) }}" class="btn btn-warning">Modify</a></td>
+               {{-- Delete --}}
+               <td>
+                  <form action="{{ route("admin.posts.destroy", $post["id"]) }}" method="post" class="d-inline-block">
+                     @csrf
+                     @method("DELETE")
+                     <button type="submit" onclick="window.confirmDelete()" class="btn btn-danger">Delete</button>
                   </form>
-                </td>
-              </tr>
-              @endforeach
-            </tbody>
-          </table>
-        </div>
-      </div>
-      
-    </div>
+               </td>
+            </tr>
+          @endforeach
+      </tbody>
+   </table>
 @endsection 
