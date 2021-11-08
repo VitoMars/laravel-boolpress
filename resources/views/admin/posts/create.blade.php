8d34093540a217d3eaec6d4608d5992438375061
Vito <vito95v@gmail.com>
2021-11-08 18:05:30 +0100

    Aggiuta sezione category al create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -21,6 +21,7 @@
                @csrf
                @method("POST")
 
+               {{-- Titolo --}}
                <div class="form-group">
                   <label for="title">Titolo</label>
                   <input type="text" name="title" class="form-control @error("title") is-invalid @enderror" value="{{ old("title") }}">
@@ -29,6 +30,7 @@
                   @enderror
                </div>
 
+               {{-- Contenuto --}}
                <div class="form-group">
                   <label for="content">Contenuto</label>
                   <textarea id="content" name="content" class="form-control @error("content") is-invalid @enderror">{{ old("content") }}</textarea>
@@ -37,6 +39,18 @@
                   @enderror
                </div>
 
+               {{-- Category --}}
+               <div class="form-group">
+                    <label for="category_id">Categoria</label>
+                    <select name="category_id" id="category_id" class="form-control">
+                        <option value="">-- Seleziona Categoria --</option>
+                        @foreach ($categories as $category)
+                            <option value="{{ $category["id"] }}">{{ $category["name"] }}</option>
+                        @endforeach
+                    </select>
+               </div>
+
+               {{-- Bottone Crea Post --}}
                <div class="form-group">
                   <button type="submit" class="btn btn-primary">Crea Post</button>
                </div>
